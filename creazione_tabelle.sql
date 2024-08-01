4b22189ec5eb35ea90c381fe74893c9b0690c7fe
MicheleDioli <micheledioli.dm@gmail.com>
2024-08-01 21:43:14 +0200

    proglogica

--- creazione_tabelle.sql
+++ creazione_tabelle.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS Contratti CASCADE;
-DROP TABLE IF EXISTS Registro_Contabile CASCADE;
-DROP TABLE IF EXISTS Dipendenti CASCADE;
-DROP TABLE IF EXISTS Ordini CASCADE:
-DROP TABLE IF EXISTS Fornitori CASCADE;
-DROP TABLE IF EXISTS Bolla_Acquisto CASCADE;
-DROP TABLE IF EXISTS Servizi_vari CASCADE;
-DROP TABLE IF EXISTS Prenotazioni CASCADE;
-DROP TABLE IF EXISTS Clienti CASCADE;
-DROP TABLE IF EXISTS Ricetta CASCADE;
 DROP TABLE IF EXISTS Registro_Vendite CASCADE;
-DROP TABLE IF EXISTS Medicinali CASCADE;
-DROP TABLE IF EXISTS Prodotti_non_Medicinali CASCADE;
+DROP TABLE IF EXISTS Vendite CASCADE;
+DROP TABLE IF EXISTS Ricetta CASCADE;
+DROP TABLE IF EXISTS Clienti CASCADE;
+DROP TABLE IF EXISTS Prenotazioni CASCADE;
+DROP TABLE IF EXISTS Servizi_vari CASCADE;
+DROP TABLE IF EXISTS Bolla_Acquisto CASCADE;
+DROP TABLE IF EXISTS Fornitori CASCADE;
+DROP TABLE IF EXISTS Ordini CASCADE;
+DROP TABLE IF EXISTS Dipendenti CASCADE;
+DROP TABLE IF EXISTS Contratti CASCADE;
+DROP TABLE IF EXISTS Prodotti CASCADE;
+DROP TABLE IF EXISTS Registro_Contabile CASCADE;
 DROP TABLE IF EXISTS Magazzino CASCADE;
 DROP TABLE IF EXISTS Turni CASCADE;
 DROP TABLE IF EXISTS Bugiardino CASCADE;
@@ -27,11 +27,23 @@ CREATE TABLE IF NOT EXISTS Contratti (
 );
 
 CREATE TABLE IF NOT EXISTS Registro_Contabile (
-    spese_ordine DOUBLE NOT NULL,
-    spese_dipendenti DOUBLE NOT NULL,
-    guadagno_vendite DOUBLE NOT NULL,
-    guadagno_altre_attivita DOUBLE NOT NULL,
+    spese_ordine DECIMAL(10, 2) NOT NULL,
+    spese_dipendenti DECIMAL(10, 2) NOT NULL,
+    guadagno_vendite DECIMAL(10, 2) NOT NULL,
+    guadagno_altre_attivita DECIMAL(10, 2) NOT NULL,
     mese_e_anno DATE PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS Prodotti (
+    id_prodotto INT PRIMARY KEY,
+    nome VARCHAR(64) NOT NULL,
+    scadenza DATE NOT NULL,
+    tipo_assunzione VARCHAR(64) NOT NULL,
+    quantita_confezione INT NOT NULL,
+    prezzo DECIMAL(10, 2) NOT NULL,
+    tipo VARCHAR(64) NOT NULL,
+    necessita_ricetta BOOLEAN NOT NULL,
+    eta_minima INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Dipendenti (
@@ -40,15 +52,18 @@ CREATE TABLE IF NOT EXISTS Dipendenti (
     certificazione VARCHAR(128) NOT NULL,
     ruolo VARCHAR(64) NOT NULL,
     posizione VARCHAR(64) NOT NULL,
-    FOREIGN KEY (numero_contratto) REFERENCES Contratti(numero_contratto)
+    id_turno INT NOT NULL,
+    FOREIGN KEY (numero_contratto) REFERENCES Contratti(numero_contratto),
+    FOREIGN KEY (id_turno) REFERENCES Turni(id_turno)
 );
 
 CREATE TABLE IF NOT EXISTS Ordini (
     id_prodotto INT PRIMARY KEY,
-    quantità INT NOT NULL,
+    quantita INT NOT NULL,
     nome_fornitore VARCHAR(64) NOT NULL,
-    prezzo_unitario DOUBLE NOT NULL,
-    data_arrivo DATE NOT NULL
+    prezzo_unitario DECIMAL(10, 2) NOT NULL,
+    data_arrivo DATE NOT NULL,
+    FOREIGN KEY (id_prodotto) REFERENCES Prodotti(id_prodotto)
 );
 
 CREATE TABLE IF NOT EXISTS Fornitori (
@@ -60,11 +75,13 @@ CREATE TABLE IF NOT EXISTS Fornitori (
 CREATE TABLE IF NOT EXISTS Bolla_Acquisto (
     nome_ditta VARCHAR(128) NOT NULL,
     quantita_prodotto INT NOT NULL,
-    prezzo_unitario DOUBLE NOT NULL,
-    prezzo_totale DOUBLE NOT NULL,
-    nome_corriere VARCHAR(64),
-    data_spedizione DATE NOT NULL,
-    modalita_spedizione VARCHAR(64)
+    data_inizio_trasporto DATE NOT NULL,
+    vettore VARCHAR(64) NOT NULL,
+    id_prodotto INT NOT NULL,
+    causale_trasporto VARCHAR(64) NOT NULL,
+    data_documento DATE NOT NULL,
+    FOREIGN KEY (id_prodotto) REFERENCES Prodotti(id_prodotto),
+    FOREIGN KEY (nome_ditta) REFERENCES Fornitori(nome_ditta)
 );
 
 CREATE TABLE IF NOT EXISTS Servizi_vari (
@@ -72,7 +89,7 @@ CREATE TABLE IF NOT EXISTS Servizi_vari (
     giorno DATE NOT NULL,
     badge_farmacista INT NOT NULL,
     fascia_orario VARCHAR(64) NOT NULL,
-    costo DOUBLE NOT NULL,
+    costo DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (badge_farmacista) REFERENCES Dipendenti(badge)
 );
 
@@ -98,38 +115,24 @@ CREATE TABLE IF NOT EXISTS Ricetta (
     FOREIGN KEY (codice_fiscale) REFERENCES Clienti(codice_fiscale)
 );
 
-CREATE TABLE IF NOT EXISTS Registro_Vendite (
-    data DATE NOT NULL,
+CREATE TABLE IF NOT EXISTS Vendite (
+    id_vendita INT PRIMARY KEY,
+    dataa DATE NOT NULL,
     codice_fiscale_cliente VARCHAR(16) NOT NULL,
     id_prodotto INT NOT NULL,
     farmacista_servente INT NOT NULL,
-    totale_spesa DOUBLE NOT NULL,
+    totale_spesa DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (codice_fiscale_cliente) REFERENCES Clienti(codice_fiscale),
-    FOREIGN KEY (id_prodotto) REFERENCES Medicinali(id_prodotto),
-    FOREIGN KEY (farmacista_servente) REFERENCES Dipendenti(badge)
+    FOREIGN KEY (farmacista_servente) REFERENCES Dipendenti(badge),
+    FOREIGN KEY (id_prodotto) REFERENCES Prodotti(id_prodotto)
 );
 
-CREATE TABLE IF NOT EXISTS Medicinali (
-    id_prodotto INT PRIMARY KEY,
-    nome VARCHAR(64) NOT NULL,
-    scadenza DATE NOT NULL,
-    tipo_assunzione VARCHAR(64) NOT NULL,
-    quantita_confezione INT NOT NULL,
-    prezzo DOUBLE NOT NULL,
-    tipo VARCHAR(64) NOT NULL,
-    necessita_ricetta BOOLEAN NOT NULL,
-    eta_minima INT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Prodotti_non_Medicinali (
-    id_prodotto INT PRIMARY KEY,
-    nome VARCHAR(64) NOT NULL,
-    scadenza DATE NOT NULL,
-    tipo_assunzione VARCHAR(64) NOT NULL,
-    quantita_confezione INT NOT NULL,
-    prezzo DOUBLE NOT NULL,
-    benefici VARCHAR(256),
-    isnaturale BOOLEAN NOT NULL
+CREATE TABLE IF NOT EXISTS Registro_Vendite(
+    id_vendita INT NOT NULL,
+    id_prodotto INT NOT NULL,
+    PRIMARY KEY (id_vendita, id_prodotto),
+    FOREIGN KEY (id_vendita) REFERENCES Vendite(id_vendita),
+    FOREIGN KEY (id_prodotto) REFERENCES Prodotti(id_prodotto)
 );
 
 CREATE TABLE IF NOT EXISTS Magazzino (
@@ -140,17 +143,20 @@ CREATE TABLE IF NOT EXISTS Magazzino (
     numero_minimo INT NOT NULL,
     data_prossimo_carico DATE,
     PRIMARY KEY (id_prodotto, fila, posto),
-    FOREIGN KEY (id_prodotto) REFERENCES Medicinali(id_prodotto)
+    FOREIGN KEY (id_prodotto) REFERENCES Prodotti(id_prodotto)
 );
 
 CREATE TABLE IF NOT EXISTS Turni (
+    id_turno INT PRIMARY KEY,
     giorni VARCHAR(64) NOT NULL,
     orario_inizio TIME NOT NULL,
     orario_fine TIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Bugiardino (
+    id_prodotto INT PRIMARY KEY,
     effetti_collaterali VARCHAR(256),
     isfans BOOLEAN NOT NULL,
-    modalita_assunzione VARCHAR(128) NOT NULL
+    modalita_assunzione VARCHAR(128) NOT NULL,
+    FOREIGN KEY (id_prodotto) REFERENCES Prodotti(id_prodotto)
 );
