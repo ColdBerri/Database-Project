84f479f660e0cfd3d76586e48eabdc9b2157bbe4
MicheleDioli <micheledioli.dm@gmail.com>
2024-08-02 22:39:01 +0200

    Aggiunto file popolazione

---
+++ popolazione_tabelle.sql
@@ -0,0 +1,2 @@
+INSERT INTO Contratti (nome, cognome, numero_contratto, codice_fiscale, data_nascita, tipologia_contratto, inizio_contratto, termine_contratto)
+VALUES ('Luciano', 'Pavarotti', 1, 'PVRLCN35R12F257P', '1935-10-12', 'Indeterminato', '2001-09-11', '2007-09-06');
