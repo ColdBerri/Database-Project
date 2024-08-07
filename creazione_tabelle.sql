29b1591cd8541913a9ae79764f017f7a9c1887ac
ColdBerrri <darkk.matter391@gmail.com>
2024-08-07 17:26:02 +0200

    mancano le ultime due tabelle da inseire con i dati, faccio stase leggere i commenti nella creazione tabelle

--- creazione_tabelle.sql
+++ creazione_tabelle.sql
@@ -23,7 +23,8 @@ CREATE TABLE IF NOT EXISTS Contratti (
     data_nascita DATE NOT NULL,
     tipologia_contratto VARCHAR(64) NOT NULL,
     inizio_contratto DATE NOT NULL,
-    termine_contratto DATE
+    termine_contratto DATE,
+    stipendio_mensile INT
 ); --dati ok
 
 CREATE TABLE IF NOT EXISTS Registro_Contabile (
@@ -32,7 +33,7 @@ CREATE TABLE IF NOT EXISTS Registro_Contabile (
     guadagno_vendite DECIMAL(10, 2) NOT NULL,
     guadagno_altre_attivita DECIMAL(10, 2) NOT NULL,
     mese_e_anno DATE PRIMARY KEY
-);
+); -- da rivedere 
 
 CREATE TYPE tipo_prodotto AS ENUM ('farmaco', 'integratore', 'cosmetico', 'attrezzatura medica', 'altro');
 
@@ -96,25 +97,16 @@ CREATE TABLE IF NOT EXISTS Bolla_Acquisto (
     data_documento DATE NOT NULL,
     FOREIGN KEY (id_prodotto) REFERENCES Prodotti(id_prodotto),
     FOREIGN KEY (nome_ditta) REFERENCES Fornitori(nome_ditta)
-);
+);--da rivedere
 
 CREATE TABLE IF NOT EXISTS Servizi_vari (
     nome_servizio VARCHAR(128) NOT NULL UNIQUE,
-    giorno DATE NOT NULL,
-    badge_farmacista INT NOT NULL,
+    giorno VARCHAR(64) NOT NULL, -- MODIFICATO DA DATE A VARCHAR PERCHÈ HO MESSO I GIORNI IN CUI È POSSIBILE RICEVERE UN SERVIZIO
+    badge_farmacista INT NOT NULL,  
     fascia_orario VARCHAR(64) NOT NULL,
     costo DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (badge_farmacista) REFERENCES Dipendenti(badge)
-);
-
-CREATE TABLE IF NOT EXISTS Prenotazioni (
-    id_prenotazione INT PRIMARY KEY,
-    giorno DATE NOT NULL,
-    orario TIME NOT NULL,
-    nome_servizio VARCHAR(128) NOT NULL,
-    nome_cliente VARCHAR(64) NOT NULL,
-    FOREIGN KEY (nome_servizio) REFERENCES Servizi_vari(nome_servizio)
-);
+);--dati ok
 
 CREATE TABLE IF NOT EXISTS Clienti (
     nome VARCHAR(64) NOT NULL,
@@ -122,12 +114,22 @@ CREATE TABLE IF NOT EXISTS Clienti (
     codice_fiscale VARCHAR(16) PRIMARY KEY,
     eta INT NOT NULL,
     CHECK (eta >= 0)
-);
+);--dati ok
+
+CREATE TABLE IF NOT EXISTS Prenotazioni (
+    id_prenotazione INT PRIMARY KEY,    
+    nome_servizio VARCHAR(128) NOT NULL,
+    giorno DATE NOT NULL,   
+    orario TIME NOT NULL,
+    cf_cliente VARCHAR(64) NOT NULL, --modificato da nome cliente a codice fiscale poichè ci possono essere omonimie ed i clienti non hanno degli id di prenotazione assegnati
+    FOREIGN KEY (nome_servizio) REFERENCES Servizi_vari(nome_servizio),
+    FOREIGN KEY (cf_cliente) REFERENCES Clienti(codice_fiscale) --aggiunta la foreign key del cf così da poter operare liberamente sulle prenotazioni
+);--dati ok
 
 CREATE TABLE IF NOT EXISTS Ricetta (
     id_ricetta INT PRIMARY KEY,
     dottore VARCHAR(64) NOT NULL,
-    nome_cliente VARCHAR(64) NOT NULL,
+    nome_cliente VARCHAR(64) NOT NULL,  
     codice_fiscale VARCHAR(16) NOT NULL,
     eta INT NOT NULL,
     FOREIGN KEY (codice_fiscale) REFERENCES Clienti(codice_fiscale)
@@ -135,7 +137,7 @@ CREATE TABLE IF NOT EXISTS Ricetta (
 
 CREATE TABLE IF NOT EXISTS Vendite (
     id_vendita INT PRIMARY KEY,
-    dataa DATE NOT NULL,
+    data_vendita DATE NOT NULL,
     codice_fiscale_cliente VARCHAR(16) NOT NULL,
     id_prodotto INT NOT NULL,
     farmacista_servente INT NOT NULL,
@@ -143,7 +145,7 @@ CREATE TABLE IF NOT EXISTS Vendite (
     FOREIGN KEY (codice_fiscale_cliente) REFERENCES Clienti(codice_fiscale),
     FOREIGN KEY (farmacista_servente) REFERENCES Dipendenti(badge),
     FOREIGN KEY (id_prodotto) REFERENCES Prodotti(id_prodotto)
-);
+); --dati ok 
 
 CREATE TABLE IF NOT EXISTS Registro_Vendite(
     id_vendita INT NOT NULL,
@@ -151,7 +153,7 @@ CREATE TABLE IF NOT EXISTS Registro_Vendite(
     PRIMARY KEY (id_vendita, id_prodotto),
     FOREIGN KEY (id_vendita) REFERENCES Vendite(id_vendita),
     FOREIGN KEY (id_prodotto) REFERENCES Prodotti(id_prodotto)
-);
+);--inutile vendite vale già come registro vendite
 
 CREATE TABLE IF NOT EXISTS Magazzino (
     id_prodotto INT NOT NULL,
